2a3ba2392c8f015ecb392cf3d5ceb2e538829773
meatroll <shiman.c@sumian.com>
2019-10-15 16:43:51 +0800

    move methods to utils.js

--- src/index.js
+++ src/index.js
@@ -2,114 +2,26 @@ const fs = require('fs')
 const path = require('path')
 const open = require('open')
 const chalk = require('chalk')
-const empty = require('empty-folder')
+
+const {readDir, checkToRun, makeDir, emptyDir, buildReportHtml} = require('./utils')
 
 const {times} = require('./configs.json')
 
-console.log(chalk.hex('#90a906')('--------------------------------------------------------------------'))
-console.log(`${chalk.hex('#90a906')('----------------------------')} ${chalk.bold.bgYellowBright.gray(' PDF TEST ')} ${chalk.hex('#90a906')('----------------------------')}`)
-console.log(chalk.hex('#90a906')('--------------------------------------------------------------------'))
-
-const checkToRun = dirPath => new Promise((resolve, reject) => {
-  fs.stat(dirPath, (e, stats) => {
-    if (e) {
-      reject(e)
-      return
-    }
-    if (!stats.isDirectory(dirPath)) {
-      resolve()
-      return
-    }
-    const indexFilePath = path.resolve(dirPath, 'index.js')
-    if (fs.existsSync(indexFilePath)) resolve(indexFilePath)
-    else resolve()
-  })
-})
-
-const mkdir = dirPath => new Promise((resolve, reject) => {
-  fs.mkdir(dirPath, e => {
-    if (e) reject(e)
-    else resolve()
-  })
-})
-
-const emptyDir = dirPath => new Promise((resolve, reject) => {
-  empty(dirPath, false, ({error, removed, failed}) => {
-    if (error) reject(error)
-    else resolve({removed, failed})
-  })
-})
-
-const writeFile = content => new Promise(async (resolve, reject) => {
-  const filePath = path.resolve(__dirname, '../temp/report.html')
-  fs.writeFile(filePath, content, e => {
-    if (e) reject(e)
-    resolve(filePath)
-  })
-})
-
-const buildReportHtml = async results => {
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>测试结果</title>
-      <style>
-        .title {
-          width: 100%;
-          height: 30px;
-          line-height: 30px;
-          font-size: 24px;
-          font-weight: bold;
-          color: #000000;
-          margin: 0;
-          padding: 15px 0;
-        }
-        .result {
-          width: 100%;
-          height: 20px;
-          line-height: 20px;
-          font-size: 18px;
-          color: #1A1A1A;
-          margin: 0;
-          padding: 5px 0;
-        }
-      </style>
-    </head>
-    <body>
-      <h3 class="title">测试结果如下：</h3>
-      ${results.map(result => `
-        <div class="result">
-          <span class="key">${result.title}：</span>
-          <span class="value">${result.time}ms</span>
-        </div>
-      `)}
-    </body>
-    </html>
-  `
-  const filePath = await writeFile(html)
-  return filePath
-}
-
-fs.readdir(__dirname, async (e, files) => {
-  if (e) {
-    console.error(e)
-    return
-  }
+const run = async () => {
+  console.log(chalk.hex('#90a906')('--------------------------------------------------------------------'))
+  console.log(`${chalk.hex('#90a906')('----------------------------')} ${chalk.bold.bgYellowBright.gray(' PDF TEST ')} ${chalk.hex('#90a906')('----------------------------')}`)
+  console.log(chalk.hex('#90a906')('--------------------------------------------------------------------'))
   const start = new Date()
   try {
+    const files = await readDir(__dirname)
     const results = []
     const tempDirPath = path.resolve(__dirname, '../temp')
-    if (!fs.existsSync(tempDirPath)) await mkdir(tempDirPath)
+    if (!fs.existsSync(tempDirPath)) await makeDir(tempDirPath)
     else await emptyDir(tempDirPath)
     for (let i = 0; i < files.length; i++) {
       const file = files[i]
       const dirPath = path.resolve(__dirname, file)
-      let filePath
-      filePath = await checkToRun(dirPath)
+      const filePath = await checkToRun(dirPath)
       if (filePath) {
         const {getResult} = require(filePath) 
         if (getResult) {
@@ -127,4 +39,6 @@ fs.readdir(__dirname, async (e, files) => {
   const time = end - start
   console.log('')
   console.log(`PDF Test Finished! Total: ${chalk.bold.redBright(time)}ms`)
-})
+}
+
+run()
---
+++ src/utils.js
@@ -0,0 +1,103 @@
+const fs = require('fs')
+const path = require('path')
+const empty = require('empty-folder')
+
+const readDir = dirPath => new Promise((resolve, reject) => {
+  fs.readdir(dirPath, (e, files) => {
+    if (e) reject(e)
+    else resolve(files)
+  })
+})
+
+const checkToRun = dirPath => new Promise((resolve, reject) => {
+  fs.stat(dirPath, (e, stats) => {
+    if (e) {
+      reject(e)
+      return
+    }
+    if (!stats.isDirectory(dirPath)) {
+      resolve()
+      return
+    }
+    const indexFilePath = path.resolve(dirPath, 'index.js')
+    if (fs.existsSync(indexFilePath)) resolve(indexFilePath)
+    else resolve()
+  })
+})
+
+const makeDir = dirPath => new Promise((resolve, reject) => {
+  fs.mkdir(dirPath, e => {
+    if (e) reject(e)
+    else resolve()
+  })
+})
+
+const emptyDir = dirPath => new Promise((resolve, reject) => {
+  empty(dirPath, false, ({error, removed, failed}) => {
+    if (error) reject(error)
+    else resolve({removed, failed})
+  })
+})
+
+const writeFile = content => new Promise(async (resolve, reject) => {
+  const filePath = path.resolve(__dirname, '../temp/report.html')
+  fs.writeFile(filePath, content, e => {
+    if (e) reject(e)
+    resolve(filePath)
+  })
+})
+
+const buildReportHtml = async results => {
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>测试结果</title>
+      <style>
+        .title {
+          width: 100%;
+          height: 30px;
+          line-height: 30px;
+          font-size: 24px;
+          font-weight: bold;
+          color: #000000;
+          margin: 0;
+          padding: 15px 0;
+        }
+        .result {
+          width: 100%;
+          height: 20px;
+          line-height: 20px;
+          font-size: 18px;
+          color: #1A1A1A;
+          margin: 0;
+          padding: 5px 0;
+        }
+      </style>
+    </head>
+    <body>
+      <h3 class="title">测试结果如下：</h3>
+      ${results.map(result => `
+        <div class="result">
+          <span class="key">${result.title}：</span>
+          <span class="value">${result.time}ms</span>
+        </div>
+      `)}
+    </body>
+    </html>
+  `
+  const filePath = await writeFile(html)
+  return filePath
+}
+
+module.exports = {
+  readDir,
+  checkToRun,
+  makeDir,
+  emptyDir,
+  writeFile,
+  buildReportHtml
+}
