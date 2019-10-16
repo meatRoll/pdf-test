b5b858c7e2a20116f107b36ba5fef7bc41154532
meatroll <shiman.c@sumian.com>
2019-10-17 04:36:19 +0800

    add left and right margin limits to wkhtmltopdf

--- src/wkhtmltopdf/index.js
+++ src/wkhtmltopdf/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const {exec} = require('child_process')
 
 const execSync = num => new Promise((resolve, reject) => {
-  exec(`${path.resolve(__dirname, '../../assets/wkhtmltopdf.exe')} -T 30MM -B 30mm --header-html ${path.resolve(__dirname, 'header.html')} ${path.resolve(__dirname, 'content.html')} --footer-html ${path.resolve(__dirname, 'footer.html')} ${path.resolve(__dirname, `../../temp/wkhtmltopdf-${num}.pdf`)}`, {
+  exec(`${path.resolve(__dirname, '../../assets/wkhtmltopdf.exe')} -L 5mm -R 5mm -T 30mm -B 30mm --header-html ${path.resolve(__dirname, 'header.html')} ${path.resolve(__dirname, 'content.html')} --footer-html ${path.resolve(__dirname, 'footer.html')} ${path.resolve(__dirname, `../../temp/wkhtmltopdf-${num}.pdf`)}`, {
     windowsHide: true
   }, e => {
     if (e) reject(e)
